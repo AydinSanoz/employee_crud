3631f1eb177ee026695e914804d299ea2c993c76
Aydin Sanoz <aydinsanoz2@gmail.com>
2023-03-16 23:02:08 +0100

    api method check added

--- src/pages/api/employee/index.js
+++ src/pages/api/employee/index.js
@@ -3,5 +3,24 @@ import connectDB from "@/database/conn";
 export default function handler(req, res) {
   connectDB().catch((err) => console.log(err));
 
-  res.status(201).json({ name: "Aydın Sanoz2" });
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      res.status(201).json({ method: "GET method" });
+      break;
+    case "POST":
+      res.status(201).json({ method: "POST method" });
+      break;
+    case "PUT":
+      res.status(201).json({ method: "PUT method" });
+      break;
+    case "DELETE":
+      res.status(201).json({ method: "DELETE method" });
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`method: ${method} is not allowed`);
+      break;
+  }
 }
