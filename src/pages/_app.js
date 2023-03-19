dbadb0b039ea7ee27afcc08f1bd042b14a6fa357
Aydin Sanoz <aydinsanoz2@gmail.com>
2023-03-19 17:53:35 +0100

    SSRProvider added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,17 @@ import Header from "@/components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import { SSRProvider } from "react-bootstrap";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Header></Header>
-        <Component {...pageProps} />
-      </Layout>
+      <SSRProvider>
+        <Layout>
+          <Header></Header>
+          <Component {...pageProps} />
+        </Layout>
+      </SSRProvider>
     </>
   );
 }
