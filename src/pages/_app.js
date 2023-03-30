b6002e0b0ac69c34c65008cfa49e12a09367ab16
Aydin Sanoz <aydinsanoz2@gmail.com>
2023-03-30 19:20:45 +0200

    redux added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,21 @@
-import Header from "@/components/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@/styles/globals.css";
-import Layout from "@/components/Layout";
 import { SSRProvider } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "@/components/Layout";
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <SSRProvider>
-        <Layout>
-          <Header></Header>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+          <Layout>
+            <Header />
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </SSRProvider>
     </>
   );
