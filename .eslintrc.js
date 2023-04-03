1a0af93f8e14180607aaf2a40704abe4b37329e1
Aydin Sanoz <aydinsanoz2@gmail.com>
2023-04-03 11:15:56 +0200

    eslint add to project

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
